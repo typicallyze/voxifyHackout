e0ecfad8100d21824453b17a6e5e1e5c047b84d3
typicallyze <umangkaushik30@gmail.com>
2023-11-04 15:17:35 +0530

    Displays the transcript on in <p> (improve later)

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,31 +4,34 @@ import OpenAI from "openai";
 
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: "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",
   dangerouslyAllowBrowser: true
 });
 
 
 export default function Home() {
   const [file, setFile] = useState<File>()
+  const [transcript, setTranscript] = useState("")
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (!file) return
 
     try {
+
       console.log("starting")
 
-      await console.log(await openai.audio.transcriptions.create({file: file,
+      setTranscript(`${await openai.audio.transcriptions.create({file: file,
         model: 'whisper-1',
         response_format: "text"  
-      }))
+      })}`)
 
       console.log("finished")
 
 
    
     } catch (e: any) {
+      // Handle errors here
       console.error(e)
     }
 
@@ -44,7 +47,7 @@ export default function Home() {
         onChange={(e) => setFile(e.target.files?.[0])}
       />
       <input type="submit" value="Upload" />
-      <p>ChatGPT</p>
+      <p>ChatGPT: {transcript}</p>
     </form>
     </main>
   )
